92e346e85a71ce6d0f28f3b8903ac8ef59cb82d9
Mauricio <darlin.amador@ujcv.edu.hn>
2019-10-30 19:03:58 -0600

    jjj

--- app/src/main/java/com/example/pokeapiclientkt/MainActivity.kt
+++ app/src/main/java/com/example/pokeapiclientkt/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.pokeapiclientkt
 
+
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokeapiclientkt.fragments.PokiListFrg
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.FirebaseException
 
 class MainActivity : AppCompatActivity() {
-    private val frgMng      = supportFragmentManager;
+    private val frgMng = supportFragmentManager;
     private val pokiListFrg = PokiListFrg(frgMng)
 
     override fun onCreate(savedInstanceState: Bundle?) {
